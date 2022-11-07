de3a7335cd1e845c3c8d9ed7df103283a3275c7f
Deepak <deepak@webllisto.com>
2022-11-07 11:18:16 +0530

    deploy5

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const app = express()
-const env = require('dotenv')
-env.config()
+
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
